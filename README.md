393c3a994bca70c5cea4648bd7c32726c1e2c9d9
noon-c <nuun0859@gmail.com>
2026-02-24 01:46:49 +0000

    feat: add HTML response for home page and implement health check endpoint

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # katz-und-maus
+
+Running on Render  
+
+Service ID: srv-d5sr9cvfte5s73ciu5hg  
+
+https://katz-und-maus.onrender.com/  
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
@@ -16,7 +15,7 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Recovery())
 
-	r.GET("/", runningPage)
+	r.GET("/", handleIndex)   // Home page
 	r.GET("/healthz", health) // Healthcheck
 
 	port := os.Getenv("PORT")
@@ -34,22 +33,4 @@ func main() {
 
 	fmt.Println("Server started: http://localhost:8080")
 	log.Printf("listening on %s", port)
-}
-
-func runningPage(c *gin.Context) {
-	c.Header("Content-Type", "text/html; charset=utf-8")
-	c.String(http.StatusOK, `<!doctype html>
-			<html lang="en">
-				<head>
-					<meta charset="utf-8">
-					<title>running...</title>
-				</head>
-				<body>
-					<h1>running...</h1>
-				</body>
-			</html>`)
-}
-
-func health(c *gin.Context) {
-	c.String(http.StatusOK, "ok")
 }
