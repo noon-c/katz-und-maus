a7f8b1bf27109ffb1f99455cdcae13386e40faad
noon-c <nuun0859@gmail.com>
2026-02-25 00:39:29 +0000

    feat: enhance password hash output with quoting and formatting

--- README.md
+++ README.md
@@ -4,4 +4,20 @@ Running on Render
 
 Service ID: srv-d5sr9cvfte5s73ciu5hg  
 
-https://katz-und-maus.onrender.com/  
+<https://katz-und-maus.onrender.com/>  
+
+## HASHES
+
+Password hashes must be quoted and screened because of $ signs.  
+
+Originally generated:
+
+```plaintext
+$2a$10$4Z3JH1KAiNoxcSqF9DdaAOnR3dATWALGwGRns09lMFFlCfKlVNNAC
+```
+
+Quoted and screened:
+
+```plantext
+"\$2a\$10\$4Z3JH1KAiNoxcSqF9DdaAOnR3dATWALGwGRns09lMFFlCfKlVNNAC"
+```
--- hash/hash.go
+++ hash/hash.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+// go get -u golang.org/x/crypto/bcrypt
+// use: go run ./hash
 func main() {
+	// pass := "A8914875"
 	pass := "123456"
 	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(hash))
+	s := string(hash)
+	s = strings.ReplaceAll(s, "$", "\\$")
+	fmt.Println("\n=== hash \n", string(hash))
+	fmt.Println("\n=== quoted hash \n\"" + s + "\" \n")
 }
